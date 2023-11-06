f9fd29a674c61c8cb7220b73cc8c3ad4e14e04ac
Nithin Rajan <rajannithin246@gmail.com>
2023-11-06 20:52:08 +0530

    excel changes added

--- Angular/src/app/hr-admin/approved-nominations/approved-nominations.component.ts
+++ Angular/src/app/hr-admin/approved-nominations/approved-nominations.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import * as ExcelJS from 'exceljs';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-approved-nominations',
@@ -9,37 +9,22 @@ import * as ExcelJS from 'exceljs';
 export class ApprovedNominationsComponent {
   dataSource:any = [];
   displayedColumns:any = [];
-  // displayedColumns: string[] = ['column1', 'column2', 'column3', 'column4', 'column5'];
-  // dataSource = [
-  //   { column1: 'Data 1', column2: 'Data 2', column3: 'Data 3', column4: 'Data 4', column5: 'Data 5' },
-  // ];
-
   onFileChange(event: any): void {
     const file = event.target.files[0];
-    const workbook = new ExcelJS.Workbook();
 
-    workbook.xlsx.load(file).then((workbook) => {
-      const worksheet = workbook.getWorksheet(1);
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const data = new Uint8Array(e.target.result);
+      const workbook = XLSX.read(data, { type: 'array' });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet);
+      const dataSource= XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      this.displayedColumns = dataSource[0];
+      this.dataSource = jsonData.slice(1);
+    };
 
-      if (worksheet) {
-        const jsonData: { [key: string]: any }[] = [];
-
-        worksheet.eachRow((row, rowNumber) => {
-          if (rowNumber !== 1) { // Skip the header row
-            const rowData: { [key: string]: any } = {};
-            row.eachCell((cell, colNumber) => {
-              rowData[`column${colNumber}`] = cell.value;
-            });
-            jsonData.push(rowData);
-          }
-        });
-
-        this.dataSource = jsonData;
-        this.displayedColumns = Object.keys(jsonData[0]);
-      } else {
-        console.error("Worksheet is undefined. Please check your Excel file.");
-      }
-    });
+    reader.readAsArrayBuffer(file);
   }
   
 
